a9332911c30dbc3f9af5e1aa7809043b4c0081a5
fatemabuarki77-spec <Fatemabuarki77@gmail.com>
2026-08-10 09:13:25 +0300

    bidpage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router";
-import { getCurrentUser, logout } from "./services/authService";
-import { useAuth } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import SignupPage from "./pages/SignupPage";
@@ -12,30 +9,59 @@ import CreateItemPage from "./pages/items/CreateItemPage";
 import ItemDetailsPage from "./pages/items/ItemDetailsPage";
 import ItemsListPage from "./pages/items/ItemsListPage";
 import UpdateItemPage from "./pages/items/UpdateItemPage";
-
+import BiddingPage from "./pages/BiddingPage";
 
 function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/items" element={<ItemsListPage />} />
 
-    return (
-        <div>
+        {/* Static /items/create placed BEFORE dynamic /items/:itemId */}
+        <Route
+          path="/items/create"
+          element={
+            <ProtectedRoute>
+              <CreateItemPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/items/:itemId" element={<ItemDetailsPage />} />
 
-            <Navbar />
-            <Routes>
+        <Route
+          path="/items/edit/:itemId"
+          element={
+            <ProtectedRoute>
+              <UpdateItemPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/items/:itemId/bid"
+          element={
+            <ProtectedRoute>
+              <BiddingPage />
+            </ProtectedRoute>
+          }
+        />
 
-                <Route path="/" element={<Homepage />} />
-                <Route path="/sign-up" element={<SignupPage />} />
-                <Route path="/sign-in" element={<SignInPage />} />
-                <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-
-                <Route path="/items" element={<ItemsListPage />} />
-                <Route path="/items/:itemId" element={<ItemDetailsPage />} />
-                <Route path="/items/create" element={<ProtectedRoute><CreateItemPage /></ProtectedRoute>} />
-                <Route path="/items/edit/:itemId" element={<ProtectedRoute><UpdateItemPage /></ProtectedRoute>} />
-
-            </Routes>
-            
-        </div>
-    );
+        {/* Dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

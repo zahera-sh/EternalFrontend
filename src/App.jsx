b4e78a859133b6320bdd177d971962e737a2b3f7
fatemabuarki77-spec <Fatemabuarki77@gmail.com>
2026-08-12 10:48:35 +0300

    Cleaning done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { Route, Routes } from "react-router";
-// imports
 import { useState, useEffect } from "react";
 
-// services
 import { getCurrentUser, logout } from "./services/authService";
 import { useAuth } from "./context/AuthContext";
 
-// components
 import Navbar from "./components/Navbar";
 import IsAdmin from "./components/IsAdmin";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-// pages
 import SignupPage from "./pages/SignupPage";
 import Homepage from "./pages/Homepage";
 import SignInPage from "./pages/SigninPage";
@@ -23,11 +19,7 @@ import ItemsListPage from "./pages/items/ItemsListPage";
 import UpdateItemPage from "./pages/items/UpdateItemPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-
 function App() {
-  // const { user } = useAuth()
-  // console.log(user)
-
   return (
     <div>
       <Navbar />
--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,29 +1,18 @@
-import api from './api';
+import api from "./api";
 
 async function getUsers() {
-    const response = await api.get(`/admin/all-users`)
-    return response.data
+  const response = await api.get(`/admin/all-users`);
+  return response.data;
 }
 
 async function deleteUser(userId) {
-    const response = await api.put(`/admin/delete/${userId}`)
-    return response.data
+  const response = await api.put(`/admin/delete/${userId}`);
+  return response.data;
 }
 
 async function verifyUser(userId) {
-    const response = await api.put(`/admin/verify/${userId}`)
-    return response.data
+  const response = await api.put(`/admin/verify/${userId}`);
+  return response.data;
 }
 
-// async function getAllBids() {
-//     const response = await api.get(`/admin/all-bids`)
-//     console.log(response.data)
-//     return response.data
-// }
-
-
-export {
-    getUsers,
-    deleteUser,
-    verifyUser
-}
+export { getUsers, deleteUser, verifyUser };
